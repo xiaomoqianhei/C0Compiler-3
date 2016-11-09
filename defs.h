eb0272fddcf5ecfc883e3437c8975c423728b0f0
TongyuYue <mycraftmw@163.com>
2016-11-09 09:50:17 +0800

    添加宏定义文件

---
+++ defs.h
@@ -0,0 +1,42 @@
+#ifndef _DEFS_H_
+#define _DEFS_H_
+
+#define CONSTSY     1
+#define INTSY       2
+#define CHARSY      3
+#define VOIDSY      4
+#define FORSY       5
+#define WHILESY     6
+#define DOSY        7
+#define IFSY        8
+#define ELSESY      9
+#define SCANSY      10
+#define PRTSY       11
+#define INTCON      12
+#define CHARCON     13
+#define STRINGCON   14
+
+#define PLUSSY      15
+#define MINUSSY     16
+#define STARSY      17
+#define DIVSY       18
+#define LCURSY      19
+#define RCURSY      20
+#define LBRKSY      21
+#define RBRKSY      22
+#define LPARSY      23
+#define RPARSY      24
+#define COMMASY     25
+#define SEMISY      26
+#define LTSY        27
+#define LESY        28
+#define GTSY        29
+#define GESY        30
+#define EQSY        31
+#define NESY        32
+
+#define INTTP       33
+#define CHARTP      34
+#define FUNCTP      35
+
+#endif
